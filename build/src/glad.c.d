ad2fdd2a4605579fef238e54e33d3c7ba75f40d8
Prixma <28676630+PedroPrisxzma@users.noreply.github.com>
2020-09-22 19:50:41 -0300

    made program window open, added all dependencies

---
+++ build/src/glad.c.d
@@ -0,0 +1,6 @@
+build/./src/glad.c.o: src/glad.c src/include/glad/glad.h \
+ src/include/KHR/khrplatform.h
+
+src/include/glad/glad.h:
+
+src/include/KHR/khrplatform.h:
---
+++ build/src/main.cpp.d
@@ -0,0 +1,18 @@
+build/./src/main.cpp.o: src/main.cpp src/include/glad/glad.h \
+ src/include/KHR/khrplatform.h src/include/game.h src/include/glad/glad.h \
+ src/include/resource_manager.h src/include/texture.h \
+ src/include/shader.h
+
+src/include/glad/glad.h:
+
+src/include/KHR/khrplatform.h:
+
+src/include/game.h:
+
+src/include/glad/glad.h:
+
+src/include/resource_manager.h:
+
+src/include/texture.h:
+
+src/include/shader.h:
